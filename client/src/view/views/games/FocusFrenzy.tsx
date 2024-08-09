2c1b545f36863cfde66a1e799834c2fa56b922eb
Enrico Scherlies <e.scherlies@pm.me>
2024-08-09 18:23:13 +0200

    Add check single todo in game

--- client/src/view/views/games/FocusFrenzy.tsx
+++ client/src/view/views/games/FocusFrenzy.tsx
@@ -1,14 +1,21 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useStore } from "../../../model/store"
 import { GetShitDoneIcon } from "../../components/Icons"
 import { SmartActionButton, Title } from "../../Ui"
-import Markdown from "../../components/Markdown"
+import { MarkdownCheckbox } from "../../components/MarkdownInteractive"
+
+import Confetti from "react-confetti"
+import { useWindowSize } from "@react-hook/window-size"
 
 type Props = { noteId: string }
 
 export const ViewFocusFrenzy = ({ noteId }: Props) => {
   const storedNotes = useStore((state) => state.notes)
   const unsyncedNewNotes = useStore((state) => state.unsyncedNewNotes)
+
+  const [showConfetti, setConfetti] = useState("no" as "no" | "yes" | "stopEmitting")
+
+  const [width, height] = useWindowSize()
 
   const notes = [...storedNotes, ...unsyncedNewNotes]
 
@@ -21,6 +28,30 @@ export const ViewFocusFrenzy = ({ noteId }: Props) => {
     setTodo(todo || null)
   }
 
+  useEffect(() => {
+    console.log("Notes changed")
+    // Check whether the note has been checked
+    if (todo && note) {
+      const todoGotChecked = todo.replace("[ ]", "[x]")
+      if (note.text.includes(todoGotChecked)) {
+        //
+        setConfetti("yes")
+        setTodo(todoGotChecked)
+        setTimeout(() => {
+          setConfetti("stopEmitting")
+          setTimeout(() => {
+            setConfetti("no")
+          }, 2000)
+        }, 3000)
+      }
+
+      const todoGotUnChecked = todo.replace("[x]", "[ ]")
+      if (note.text.includes(todoGotUnChecked)) {
+        setTodo(todoGotUnChecked)
+      }
+    }
+  }, [note?.text, todo])
+
   if (!note) {
     return (
       <div className="flex h-full w-full">
@@ -31,6 +62,8 @@ export const ViewFocusFrenzy = ({ noteId }: Props) => {
 
   const todos = extractMarkdownCheckboxes(note.text)
 
+  console.log(showConfetti)
+
   return (
     <div className="flex flex-col gap-5">
       <Title>Focus Frenzy</Title>
@@ -38,14 +71,22 @@ export const ViewFocusFrenzy = ({ noteId }: Props) => {
       <p>Go set yourself a timer. We recommentd an hour.</p>
       <SmartActionButton
         onClick={() => getRandomTodo(todos)}
-        text="Ok lets get shit done!"
+        text="Let's go!"
         icon={<GetShitDoneIcon />}
       />
 
       {todo && (
         <div className="p-5 bg-background-secondary rounded-xl">
-          <Markdown className="text-2xl" content={todo}></Markdown>
+          <MarkdownCheckbox markdown={todo} noteId={noteId} />
         </div>
+      )}
+      {showConfetti !== "no" && (
+        <Confetti
+          numberOfPieces={showConfetti == "yes" ? 200 : 0}
+          className="fixed inset-0 overflow-hidden"
+          width={width - 1}
+          height={height - 1}
+        />
       )}
     </div>
   )
@@ -53,7 +94,7 @@ export const ViewFocusFrenzy = ({ noteId }: Props) => {
 
 function extractMarkdownCheckboxes(markdownText: string): string[] {
   // Regular expression to match markdown checkboxes
-  const checkboxRegex = /- \[( |x)\]\s.*$/gm
+  const checkboxRegex = /- \[ \]\s.*$/gm
 
   // Match all checkboxes in the markdown text
   const matches = markdownText.match(checkboxRegex)
